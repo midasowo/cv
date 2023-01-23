a9d24c9b42368200ef4299c15247d923da720e6f
midasowo <kmiduch@gmail.com>
2023-01-23 17:49:06 +0100

    initial commit

---
+++ src/main/java/com/example/mycv/MyCvApplication.java
@@ -0,0 +1,13 @@
+package com.example.mycv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyCvApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyCvApplication.class, args);
+    }
+
+}
