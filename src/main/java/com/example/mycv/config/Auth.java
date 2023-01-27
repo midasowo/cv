bb81f0e908a9c39398ac6649bc5a76013f3ad04f
midasowo <kmiduch@gmail.com>
2023-01-27 17:35:02 +0100

    updated spring security

---
+++ src/main/java/com/example/mycv/config/Auth.java
@@ -0,0 +1,64 @@
+package com.example.mycv.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractAuthenticationFilterConfigurer;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.web.SecurityFilterChain;
+
+import java.util.Arrays;
+
+@Configuration
+@EnableWebSecurity
+public class Auth {
+
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
+    }
+
+
+    @Bean
+    public InMemoryUserDetailsManager get() {
+        UserDetails user = User.withUsername("test")
+                .password(passwordEncoder().encode("test"))
+                .roles("USER")
+                .build();
+        UserDetails admin = User.withUsername("admin")
+                .password(passwordEncoder().encode("admin"))
+                .roles("ADMIN")
+                .build();
+        return new InMemoryUserDetailsManager(Arrays.asList(user, admin));
+    }
+
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http.authorizeRequests()
+                .antMatchers("/")
+                .permitAll()
+                .antMatchers("/editAbout/**", "/editContact/**", "/editEducation/**", "/editExperience/**", "/editInterests/**", "/editSkills/**")
+                .hasAnyRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+                .anyRequest()
+                .authenticated()
+                .and()
+                .csrf().disable()
+                .headers().frameOptions().disable()
+                .and()
+                .formLogin()
+                .permitAll()
+                .and()
+                .logout()
+                .logoutUrl("/logout")
+                .permitAll();
+        return http.build();
+    }
+}
